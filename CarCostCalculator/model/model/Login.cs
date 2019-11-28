63e5f8d435a11998275e4530805866473b0ca319
ThomasCPNV <Thomas.huguet@cpnv.ch>
2019-11-28 12:04:17 +0100

    Création DBConnect + Login
    début du codage de DBConnect + Login (non fini)

--- CarCostCalculator/model/model/Login.cs
+++ CarCostCalculator/model/model/Login.cs
@@ -8,5 +8,26 @@ namespace model
 {
     public class Login
     {
+        private string email;
+        private string password;
+        private DBConnect dbconnect = new DBConnect();
+
+        public bool isLoginCorrect(string email, string password)
+        {
+            bool isLoginCorrect = false;
+
+            string checkLogin = dbconnect.CheckLogin(email, password);
+
+                if (checkLogin == "")
+                {
+                    isLoginCorrect = false;
+                }
+                else
+                {
+                    isLoginCorrect = true;
+                }
+
+            return isLoginCorrect;
+        }
     }
 }
