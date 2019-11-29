fc00d2c3c024fe2fc8b5ff2d589a94bf13b7df6a
Dr.Souka <samuels.meyer@gmail.com>
2019-11-29 14:42:20 +0100

    Retour à une version ultérieure du Projet
    version du 29.11.2019 10h15

---
+++ CarCostCalculator/model/Register.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model
+{
+    class Register
+    {
+        private DBConnect dbConnect = new DBConnect();
+
+        public bool RegisterNewAccount(string email, string password, string passwordConfirmation)
+        {
+            bool isRegisterOK = false;
+
+            string passwordHashed = "";
+
+            if (passwordConfirmation == password)
+            {
+                dbConnect.InsertUser(email, passwordHashed);
+                isRegisterOK = true;
+            }
+
+            return isRegisterOK;
+        }
+    }
+}
