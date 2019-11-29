8facb3fe602ee1cff8b91dd951e9618947c2b872
Dr.Souka <samuels.meyer@gmail.com>
2019-11-29 08:38:36 +0100

    Changement entre Login et Register
    Ajout d'un bouton permettant de changer de fenêtre de Login à Register et de Register à Login.

---
+++ CarCostCalculator/view/FormLoginRegister.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace view
+{
+    public partial class FormLoginRegister : Form
+    {
+        public FormLoginRegister()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void btnLoginRegisterChange_Click(object sender, EventArgs e)
+        {
+            if (lblConfirm.Visible == true)
+            {
+                lblConfirm.Visible = false;
+                txtConfirm.Visible = false;
+
+                btnSubmit.Text = "Sign In";
+                btnChange.Text = "Sign Up";
+            }
+            else
+            {
+                lblConfirm.Visible = true;
+                txtConfirm.Visible = true;
+
+                btnSubmit.Text = "Sign Up";
+                btnChange.Text = "Sign In";
+            }
+        }
+    }
+}
