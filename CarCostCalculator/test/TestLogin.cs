ed119270840bdd12c1d027df65397a43fe9ae5ab
ThomasCPNV <Thomas.huguet@cpnv.ch>
2019-12-05 12:03:50 +0100

    Test Login / Register Partie 1

--- CarCostCalculator/test/TestLogin.cs
+++ CarCostCalculator/test/TestLogin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using model;
 
 namespace test
 {
@@ -7,8 +8,44 @@ namespace test
     public class TestLogin
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestLoginCorrect()
         {
+            Login login = new Login();
+            Register register = new Register();
+
+            try
+            {
+                register.RegisterNewAccount("Thomas.Huguet@cpnv.ch", "Pa$$w0rd", "Pa$$w0rd");
+            }
+            catch
+            {
+                
+            }
+
+            bool loginVerify = login.IsLoginCorrect("Thomas.Huguet@cpnv.ch", "Pa$$w0rd");
+
+            Assert.AreEqual(loginVerify, true);
         }
+
+        [TestMethod]
+        public void TestLoginEmailIncorrect()
+        {
+            Login login = new Login();
+
+            bool loginVerify = login.IsLoginCorrect("Thomas.Huget@cpnv.ch", "Pa$$w0rd");
+
+            Assert.AreEqual(loginVerify, false);
+        }
+
+        [TestMethod]
+        public void TestLoginPasswordIncorrect()
+        {
+            Login login = new Login();
+
+            bool loginVerify = login.IsLoginCorrect("Thomas.Huguet@cpnv.ch", "Password");
+
+            Assert.AreEqual(loginVerify, false);
+        }
+
     }
 }
---
+++ CarCostCalculator/test/TestRegister.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using model;
+
+namespace test
+{
+    [TestClass]
+    public class TestRegister
+    {
+        [TestMethod]
+        public void TestRegisterCorrect()
+        {
+            Register register = new Register();
+            Random random = new Random();
+
+            int randomNumber = random.Next();
+
+            bool registerVerify = register.RegisterNewAccount($"test_{randomNumber}", "Pa$$W0rd", "Pa$$W0rd");
+
+            Assert.AreEqual(registerVerify, true);
+        }
+
+        [TestMethod]
+        public void TestRegisterIncorrect()
+        {
+            Register register = new Register();
+
+            bool registerVerify = register.RegisterNewAccount($"Thomas.Huguet@cpnv.ch", "Pa$$w0rd", "Pa$$w0rd");
+
+            Assert.AreEqual(registerVerify, false);
+        }
+
+    }
+}
