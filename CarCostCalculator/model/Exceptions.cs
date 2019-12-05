d37970b02def262c6cf0b878741464753e5b74f0
ThomasCPNV <thomas.huguet@cpnv.ch>
2019-12-05 18:38:25 +0100

    Exceptions

--- CarCostCalculator/model/Exceptions.cs
+++ CarCostCalculator/model/Exceptions.cs
@@ -6,21 +6,40 @@ using System.Threading.Tasks;
 
 namespace model
 {
-    class Exceptions
+    public class Exceptions
     {
+        public partial class ConnetionDB : Exception
+        {
+            public string messageError = "Connexion à la base de données impossible !";
+        }
+
         public class EmailAlreadyTaken : Exception
         {
+            public string messageError = "Cet email est déjà utilisé !";
         }
 
         public class PasswordNotLikeConfirmPassword : Exception
         {
+            public string messageError = "La confirmation de mot de passe n'est pas égal au mot de passe !";
+        }
+
+        public class FormatEmail : Exception
+        {
+            public string messageError = "Le format de l'email n'est pas respecté !";
+        }
+
+        public class WeakPassword : Exception
+        {
+            public string messageError = "Votre mot de passe est trop faible !";
         }
 
         public class WrongPassword : Exception
         {
+            public string messageError = "Mauvais mot de passe !";
         }
-        public class WrongEmail : Exception
+        public class UnknowEmail : Exception
         {
+            public string messageError = "Email inconnu !";
         }
 
     }
