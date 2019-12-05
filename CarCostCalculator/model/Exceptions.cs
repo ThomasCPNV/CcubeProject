40ca2d71a68f0659f704efb88dc38ad23d348b15
ThomasCPNV <Thomas.huguet@cpnv.ch>
2019-12-05 15:05:43 +0100

    Création du fichier Exceptions

---
+++ CarCostCalculator/model/Exceptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model
+{
+    class Exceptions
+    {
+        public class EmailAlreadyTaken : Exception
+        {
+        }
+
+        public class PasswordNotLikeConfirmPassword : Exception
+        {
+        }
+
+        public class WrongPassword : Exception
+        {
+        }
+        public class WrongEmail : Exception
+        {
+        }
+
+    }
+}
