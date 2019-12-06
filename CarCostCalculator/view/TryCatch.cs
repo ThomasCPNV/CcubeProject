be9a431b3dd8b5d6caf842b8926cb52d71ddcf2e
Dr.Souka <samuels.meyer@gmail.com>
2019-12-06 08:34:42 +0100

    View ajout de la classe "TryCtach"

---
+++ CarCostCalculator/view/TryCatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace view
+{
+    public class TryCatch
+    {
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                MessageBox.Show("This isn't an email !");
+                return false;
+            }
+        }
+
+        public bool IsGoodPassword(string password)
+        {
+            try
+            {
+                return password.Length >= 8;
+            }
+            catch
+            {
+                MessageBox.Show("The password must be at least eight characters long !");
+                return false;
+            }
+        }
+    }
+}
